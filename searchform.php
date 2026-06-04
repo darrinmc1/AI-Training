01c93798ca9c7863e5d9619f163da11731a89705
Darrin M <darrinmc1@yahoo.com>
2026-06-04 23:07:59 +1000

    chore: remove 29 WordPress PHP files from Next.js project
    Accidentally added WordPress theme files (archive.php, page.php,
    single.php, header.php, functions.php, etc.) from the old site.
    These have no place in the Next.js app.

--- searchform.php
+++
@@ -1,5 +0,0 @@
-<form method="get" id="searchform" class="form__grup" action="<?php bloginfo('url'); ?>">
-<div>
-<input class="text" type="text" value="<?php echo get_search_query(); ?>" placeholder="Search …" name="s" id="s" />
-</div>
-</form>
